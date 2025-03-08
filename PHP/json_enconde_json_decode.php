acc88b954d01538ccdd7abf903f21ed16d4631ed
ivdev04 <101958046+Ivan04dev@users.noreply.github.com>
2025-03-08 11:23:46 -0600

    Add files via upload

---
+++ PHP/json_enconde_json_decode.php
@@ -0,0 +1,35 @@
+<?php
+    # Arreglo asociativo
+    $productos = [
+        [
+            'nombre' => 'Tablet',
+            'precio' => 200,
+            'disponible' => true
+        ],
+        [
+            'nombre' => 'Television 24"',
+            'precio' => 300,
+            'disponible' => true
+        ],
+        [
+            'nombre' => 'Monitor Curvo',
+            'precio' => 400,
+            'disponible' => false
+        ]
+    ];
+
+    
+    foreach($productos as $producto){ ?>
+        <ul>
+            <li>
+                <p>Producto: <?php echo $producto['nombre']; ?></p>
+            </li>
+            <li>
+                <p>Precio: <?php echo $producto['precio']; ?></p>
+            </li>
+            <li>
+                <p><?php echo ($producto['disponible']) ? 'Disponible' : 'No Disponible'; ?></p>
+            </li>
+        </ul>
+    <?php }
+?>
